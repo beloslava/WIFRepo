4d352b17537661145ddb5a73507e8b6ab4a6749b
Iliyana Vitanova <iliyanavitanova96@gmail.com>
2016-09-17 00:05:04 +0300

    fix registerservlet

--- src/controller/RegisterServlet.java
+++ src/controller/RegisterServlet.java
@@ -3,13 +3,14 @@ package controller;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.HashSet;
-import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,42 +19,50 @@ import javax.servlet.http.Part;
 
 import model.pojo.UsersManager;
 
-/**
- * Servlet implementation class RegisterServlet
- */
 @WebServlet("/RegisterServlet")
+@MultipartConfig
 public class RegisterServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String email = request.getParameter("email");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String name = request.getParameter("name");
 		String password = request.getParameter("password");
 		String password2 = request.getParameter("password2");
-		String name = request.getParameter("name");
 		int age = Integer.parseInt(request.getParameter("age"));
-		String gender=request.getParameter("gender");
-		String description=request.getParameter("description");
-		Part avatar = request.getPart("profilePic");//handles data from <input type=file name=profilePic>
-		InputStream profilePicStream = avatar.getInputStream();
-		
-		Pattern pattern = Pattern.compile("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
+		String gender = request.getParameter("gender");
+		String email = request.getParameter("email");
+		String about = request.getParameter("about");
+		Part avatar = request.getPart("file");
+		InputStream avatarStream = avatar.getInputStream();
+		Pattern pattern = Pattern.compile(
+				"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
 		Matcher mattcher = pattern.matcher(email);
-		String html="";
-		if((mattcher.matches())&& (!email.isEmpty()) && (!password.isEmpty()) && (password.equals(password2)) &&(!name.isEmpty())&&(age>0)&&(gender.equals("female")||gender.equals("male"))){
+		String html = "";
+		if ((mattcher.matches()) && (!email.isEmpty()) && (!password.isEmpty()) && (password.equals(password2))
+				&& (!name.isEmpty()) && (age > 0)
+				&& (gender.equalsIgnoreCase("female") || gender.equalsIgnoreCase("male"))) {
 			File dir = new File("userProfilePics");
-			if(!dir.exists()){
+			if (!dir.exists()) {
 				dir.mkdir();
 			}
-			File avatarFile = new File(dir, name+"-profile-pic."+ avatar.getContentType().split("/")[1]);
-			UsersManager.getInstance().regUser( email, password, name,age, gender, description,avatarFile.getName() , new HashSet<>());
+			File avatarFile=new File(dir, name + "-profile-pic." + avatar.getContentType().split("/")[1]);
+			if(!avatarFile.exists()){
+				avatarFile.createNewFile();
+			}			
+
+			Files.copy(avatarStream, avatarFile.toPath());
+			System.out.println("Try to save file with name: " + avatarFile.getName());
+			System.out.println("abs. path = " + avatarFile.getAbsolutePath());
+			UsersManager.getInstance().regUser(email, password, name, age, gender, about, avatarFile.getName(),
+					new HashSet<>());
 			html="index.html";
-		}else{
+		}
+		else{
 			html="RegisterFailed.html";
 		}
-			RequestDispatcher view = request.getRequestDispatcher(html);
-			view.forward(request, response);
-		
-	
+
+		RequestDispatcher view = request.getRequestDispatcher(html);
+		view.forward(request, response);
 	}
 
 }
